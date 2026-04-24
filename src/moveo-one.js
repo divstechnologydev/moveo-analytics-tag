c8836844cbaac5732391e10a42f1b035d19a2598
Pavle Rogan <pavle@divstechnology.com>
2026-04-24 11:17:47 +0200

    ver 20

--- src/moveo-one.js
+++ src/moveo-one.js
@@ -4,7 +4,7 @@
   
     const API_URL = "{{API_URL}}";
     const DOLPHIN_URL = "{{DOLPHIN_URL}}";
-    const LIB_VERSION = "1.0.22"; // Constant library version - cannot be changed by client
+    const LIB_VERSION = "1.0.20"; // Constant library version - cannot be changed by client
     const LOGGING_ENABLED = false; // Enable/disable console logging
 
     const REDACTED_VALUE = "[REDACTED]";
